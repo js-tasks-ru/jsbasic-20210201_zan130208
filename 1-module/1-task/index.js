e2dc214819d2c31bee68716b2ef4f29cc85ca7d3
Zoja <zan130208@ya.ru>
2021-02-09 03:48:12 +0300

    исправила решение 1_module_task_1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,9 +4,11 @@
  * @returns {number}
  */
 function factorial(n) {
-  let res=1;
-  while (n--)
-  res *= n + 1
+  let res = 1;
+  while(n>0) {
+    res = res * n;
+    n = n - 1;
+  }
   return res;
+
 }
-alert(factorial(1, 3, 5));
