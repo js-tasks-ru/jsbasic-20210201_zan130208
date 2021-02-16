8b4cd61c91228adc700589f64ad7da435254838f
Zoja <zan130208@ya.ru>
2021-02-16 04:32:01 +0300

    2_module_task_2 & 3_module_tasks_1_2_3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,26 @@
 function camelize(str) {
-  // ваш код...
+
+  let arr = str.split("-");
+
+  let newStr = "";
+
+  for (let i = 0; i < arr.length; i++) {
+
+    arr[i] = arr[i].toLowerCase();
+
+    if(i > 0) {
+      let upperCaseMod = arr[i].charAt(0).toUpperCase();
+
+      arr[i] = upperCaseMod + arr[i].slice(1);
+
+    }
+
+    newStr += arr[i];
+
+  }
+
+  alert (newStr);
+  return newStr;
 }
+
+alert (camelize(""));
