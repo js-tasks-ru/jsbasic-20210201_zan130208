dcd927ad42cab03d114b96e61198de73563c56fc
Zoja <zan130208@ya.ru>
2021-02-18 07:53:48 +0300

    4_module_tasks_1_2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,15 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+
+  for (let i = 0; i < table.rows.length; i++) {
+    let element = table.rows[i];
+    element.cells[i].style.backgroundColor = "red";
+  }
+
+
+  // table.rows [0].cells[0]
+  // table.rows [1].cells[1]
+  // table.rows [2].cells[2]
+  // table.rows [3].cells[3]
+  // table.rows [4].cells[4]
+
 }
