01c5b0081f4b0ddba45779028e625628aabe0b3a
Zoja <zan130208@ya.ru>
2021-04-01 22:58:07 +0300

    module_7_tasks_2&3

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,57 +2,59 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
-    this.elem = this.render();
-    this.closeButton = this.elem.querySelector('.modal__close');
-    this.container = document.querySelector('.container');
-    this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.close = this.close.bind(this);
+    this.render();
+    this.elem.addEventListener('click', (event) => this.onClick(event));
   }
-
-  render () {
-    const modal = createElement(`
-    <div class="modal">
-    <div class="modal__overlay"></div>
-    <div class="modal__inner">
-      <div class="modal__header">
-        <button type="button" class="modal__close">
-          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
-        </button>
-        <h3 class="modal__title">
-        </h3>
+  render() {
+    this.elem = createElement(`
+      <div class="modal">
+        <div class="modal__overlay"></div>
+        <div class="modal__inner">
+          <div class="modal__header">
+            <button type="button" class="modal__close">
+              <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+            </button>
+            <h3 class="modal__title"></h3>
+          </div>
+          <div class="modal__body"></div>
+        </div>
       </div>
-      <div class="modal__body">
-      </div>
-    </div></div>`);
-    return modal;
+    `);
   }
-
-  open () {
-    this.container.append(this.elem);
+  sub(ref) {
+    return this.elem.querySelector(`.modal__${ref}`);
+  }
+  open() {
+    document.body.append(this.elem);
     document.body.classList.add('is-modal-open');
-
-    document.addEventListener('keydown', this.handleKeyDown);
-    this.closeButton.addEventListener('click', this.close);
+    this._keydownEventListener = (event) => this.onDocumentKeyDown(event);
+    document.addEventListener('keydown', this._keydownEventListener);
+    if (this.elem.querySelector('[autofocus]')) {
+      this.elem.querySelector('[autofocus]').focus();
+    }
   }
-
-  handleKeyDown(event) {
-    if (event.code === 'Escape') {
+  onClick(event) {
+    if (event.target.closest('.modal__close')) {
+      event.preventDefault();
       this.close();
     }
   }
-
+  onDocumentKeyDown(event) {
+    if (event.code === 'Escape') {
+      event.preventDefault();
+      this.close();
+    }
+  }
   setTitle(title) {
-    const modalTitle = this.elem.querySelector('.modal__title');
-    modalTitle.innerHTML = title;
+    this.sub('title').textContent = title;
   }
-
-  setBody(elem) {
-    const modalBody = this.elem.querySelector('.modal__body');
-    modalBody.replaceWith(elem);
+  setBody(node) {
+    this.sub('body').innerHTML = '';
+    this.sub('body').append(node);
   }
-
   close() {
+    document.removeEventListener('keydown', this._keydownEventListener);
     document.body.classList.remove('is-modal-open');
-    this.container.remove(this.elem);
+    this.elem.remove();
   }
 }
