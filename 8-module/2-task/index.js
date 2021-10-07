7a13bd9f1f397466bd1b8876d1d60bb24c63b5e6
Zoja <zan130208@ya.ru>
2021-10-08 01:06:54 +0300

    8 & 9 tasks

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,38 +5,75 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
-    this.elem = this.render();
+    this.render();
   }
 
   render() {
-    const productsGrid = createElement(`
+    this.elem = createElement(`
     <div class="products-grid">
-    <div class="products-grid__inner">${this.renderCards()}
+    <div class="products-grid__inner">
     </div>
     </div>`);
-    return productsGrid;
+    this.renderCards();
   }
 
-  renderCards = () => {
-    const cards = this.products.map((product) => {
-      const newProduct = new ProductCard(product);
-      return newProduct.elem.innerHTML;
-    });
+  renderCards() {
+    const inner = this.elem.querySelector('.products-grid__inner');
+    inner.innerHTML = '';
 
-    this.updateFilter(this.filters);
+    for (let product of this.products) {
+      if (this.filters.noNuts && product.nuts) {continue;}
+      if (this.filters.vegeterianOnly && !product.vegeterian) {continue;}
+      if (this.filters.maxSpiciness !== undefined && product.spiciness > this.filters.maxSpiciness) {
+        continue;}
+      if (this.filters.category && product.category != this.filters.category) {continue;}
 
-    return cards.join('');
+      const card = new ProductCard(product);
+      inner.append(card.elem);
+    }
   }
 
   updateFilter(filters) {
-    // console.log({filters}); отрабатывает
-    // console.log('this.products', this.products); отрабатывает, возвращает массив
-    // console.log('products', products); не находит
-
-    return cards = products.filter((item) => item.spiciness <= filters.maxSpiciness &&
-      item.nuts !== filters.noNuts &&
-      item.vegeterian == filters.vegeterianOnly &&
-      item.category == filters.category
-    );
+    Object.assign(this.filters, filters);
+    this.renderCards();
   }
 }
+
+// constructor(products) {
+  //   this.products = products;
+  //   this.filters = {};
+  //   this.elem = this.render();
+  // }
+
+  // render() {
+  //   const productsGrid = createElement(`
+  //   <div class="products-grid">
+  //   <div class="products-grid__inner">${this.renderCards()}
+  //   </div>
+  //   </div>`);
+  //   return productsGrid;
+  // }
+
+  // renderCards = () => {
+  //   const cards = this.products.map((product) => {
+  //     const newProduct = new ProductCard(product);
+  //     return newProduct.elem.innerHTML;
+  //   });
+
+  //   this.updateFilter(this.filters);
+  //   return cards.join('');
+  // }
+
+  // updateFilter(filters) {
+  //   Object.assign(this.filters, filters);
+  //   this.renderCards();
+  // //   // console.log({filters}); //отрабатывает
+  // //   // console.log('this.products', this.products); // отрабатывает, возвращает массив
+  // //   // НЕ ФИЛЬТРУЕТ НА СТРАНИЦЕ
+
+  //   // return this.cards = this.products.filter((item) => item.spiciness <= filters.maxSpiciness &&
+  //   //   item.nuts !== filters.noNuts &&
+  //   //   item.vegeterian == filters.vegeterianOnly &&
+  //   //   item.category == filters.category
+  //   // );
+  // }
